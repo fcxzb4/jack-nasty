acddbd6c7eacb709e6decaecf0563c810cd5c7e5
Bridget <84099228+fcxzb4@users.noreply.github.com>
2025-11-06 21:31:47 -0300

    sd

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [],
+  imports: [
+    PassportModule.register({defaultStrategy: "jwt"}),
+    JwtModule.register({
+      secret:"PELO_AMOR_DE_DEUS_SEU_JOAO_1234",
+      signOptions:{
+        expiresIn:'1h'
+      }
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
